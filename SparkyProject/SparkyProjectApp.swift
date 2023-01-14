c993a6e50d31562da52c9165d5a5b868c29e88d1
raghad <raghad.k.alsaif@gmail.com>
2023-01-15 01:03:18 +0300

    Initial Commit

---
+++ SparkyProject/SparkyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkyProjectApp.swift
+//  SparkyProject
+//
+//  Created by raghad khalid alsaif on 22/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct SparkyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
